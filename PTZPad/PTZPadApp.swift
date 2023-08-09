e35889302499cb567e8b6cf362490674537b2d64
Taikhoom Attar <tattar8@me.com>
2023-08-09 14:55:36 -0400

    Initial Commit

---
+++ PTZPad/PTZPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTZPadApp.swift
+//  PTZPad
+//
+//  Created by Taikhoom Attar on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PTZPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
